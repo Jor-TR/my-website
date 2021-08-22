537b9bef1cd1ce1b4af55b27100229bf79cb54a0
Joe-jay <602986767@qq.com>
2021-08-22 15:01:05 +0800

    hello world by java

---
+++ java_server/src/test/java/com/joe/box/myblog/MyblogApplicationTests.java
@@ -0,0 +1,11 @@
+package com.joe.box.myblog;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
